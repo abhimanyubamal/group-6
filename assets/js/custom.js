df1031fdea0668928d5df10ea3c0f021ed1b56e7
abamal <abamal@uoguelph.ca>
2023-09-19 14:59:14 +0530

    header responsive complete

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,5 +1,8 @@
 let label = document.querySelector("#label");
+let navList = document.querySelector(".nav-list");
 
 label.addEventListener("click", () => {
     label.classList.toggle("active");
+    document.body.classList.toggle("overflow-y-hidden");
+    navList.classList.toggle("active");
 })
